b314ed6e8ae51843e66a9525663d2541473bdad8
Tarun-ctrl-ops <tarungopi2712@gmail.com>
2026-01-09 22:59:28 +0530

    Fixed Expert.js file

--- frontend/experthour-frontend/src/api/expertApi.js
+++ frontend/experthour-frontend/src/api/expertApi.js
@@ -1,11 +1,4 @@
 import api from "./client";
 
-export const getAllExperts = async () => {
-  const res = await api.get("/experts");
-  return res.data;
-};
-
-export const createExpert = async (data) => {
-  const res = await api.post("/experts", data);
-  return res.data;
-};
+export const getExperts = () => api.get("/experts");
+export const createExpert = (data) => api.post("/experts", data);
