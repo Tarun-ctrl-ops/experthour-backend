96de6bd6d24a545a0b021b6fb9bbdb96c4a7663d
Tarun-ctrl-ops <tarungopi2712@gmail.com>
2026-01-10 19:33:01 +0530

    modified frontend url to vercel

--- frontend/experthour-frontend/src/api/client.js
+++ frontend/experthour-frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const client = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: import.meta.env.VITE_API_URL + "/api"
 });
 
 client.interceptors.request.use(config => {
