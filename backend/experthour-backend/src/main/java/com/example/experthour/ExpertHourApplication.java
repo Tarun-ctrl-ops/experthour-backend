819bdc7a3b76b6f552fac15c35496dc4bae3eef4
Tarun-ctrl-ops <tarungopi2712@gmail.com>
2026-01-13 02:26:49 +0530

    fixed postgres properties

--- backend/experthour-backend/src/main/java/com/example/experthour/ExpertHourApplication.java
+++ backend/experthour-backend/src/main/java/com/example/experthour/ExpertHourApplication.java
@@ -3,9 +3,7 @@ package com.example.experthour;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
-})
+@SpringBootApplication
 public class ExpertHourApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpertHourApplication.class, args);
